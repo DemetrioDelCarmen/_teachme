5a6815aef7257b635807a46757b6ab19338d3476
demetriodelcarmen <demeritho95@gmail.com>
2019-11-26 23:20:24 -0600

    vista de login terminada

---
+++ app/src/main/java/mx/edu/utez/teachme/ui/login/Login.kt
@@ -0,0 +1,13 @@
+package mx.edu.utez.teachme.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mx.edu.utez.teachme.R
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
